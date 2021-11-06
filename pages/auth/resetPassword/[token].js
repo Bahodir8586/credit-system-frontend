7d55f10e3e5814c183de73000effdca750688818
Bahodir8586 <bahodira213@gmail.com>
2021-11-06 14:07:18 +0500

    reset password page

--- pages/auth/resetPassword/[token].js
+++ pages/auth/resetPassword/[token].js
@@ -1,3 +1,24 @@
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+
+import Reset from '@/modules/shared/ResetPassword';
+import axios from '@/utils/axios';
+
 export default function ResetPassword() {
-  return <></>;
+  const [errorMessage, setErrorMessage] = useState('');
+  const router = useRouter();
+  const resetPasswordHandler = (password, passwordConfirm) => {
+    const token = router.query.token;
+    axios
+      .post(`/users/resetPassword/${token}`, { password, passwordConfirm })
+      .then((response) => {
+        console.log(response);
+        router.push('/');
+      })
+      .catch((error) => {
+        console.log(error.response?.data?.message);
+        setErrorMessage(error.response?.data?.message);
+      });
+  };
+  return <Reset submitForm={(p, pc) => resetPasswordHandler(p, pc)} errorMessage={errorMessage} />;
 }
