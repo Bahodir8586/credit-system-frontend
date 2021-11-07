ab1049140285189ce21c634d065dd77ec742337a
Bahodir8586 <bahodira213@gmail.com>
2021-11-07 13:46:59 +0500

    blocking routes

--- pages/auth/forgotPassword.js
+++ pages/auth/forgotPassword.js
@@ -4,6 +4,33 @@ import { useRouter } from 'next/router';
 import Forgot from '@/modules/shared/ForgotPassword';
 import axios from '@/utils/axios';
 
+export async function getServerSideProps(context) {
+  const cookies = context.req.headers.cookie?.split('; ').reduce((prevValue, currentValue) => {
+    const key = currentValue.split('=')[0];
+    prevValue[key] = currentValue.split('=')[1];
+    return prevValue;
+  }, {});
+  if (cookies?.jwt) {
+    const response = await fetch('http://localhost:5000/api/users', {
+      headers: {
+        Authorization: `Bearer ${cookies.jwt}`,
+      },
+    });
+    const data = await response.json();
+    if (data.status === 'error' || data.status === 'fail') {
+      // Clear the cookie and permit to access
+      return { props: {} };
+    }
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    };
+  }
+  return { props: {} };
+}
+
 export default function ForgotPassword() {
   const [errorMessage, setErrorMessage] = useState('');
   const router = useRouter();
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,38 @@
-import Head from 'next/head'
+import routePaths from '@/route-paths';
+import Head from 'next/head';
+
+export async function getServerSideProps(context) {
+  const cookies = context.req.headers.cookie?.split('; ').reduce((prevValue, currentValue) => {
+    const key = currentValue.split('=')[0];
+    prevValue[key] = currentValue.split('=')[1];
+    return prevValue;
+  }, {});
+  if (!cookies?.jwt) {
+    return {
+      redirect: {
+        destination: routePaths.signin,
+        permanent: false,
+      },
+    };
+  }
+  const response = await fetch('http://localhost:5000/api/users', {
+    headers: {
+      Authorization: `Bearer ${cookies.jwt}`,
+    },
+  });
+  const data = await response.json();
+  if (data.status === 'error' || data.status === 'fail') {
+    return {
+      redirect: {
+        destination: routePaths.signin,
+        permanent: false,
+      },
+    };
+  }
+  return {
+    props: {},
+  };
+}
 
 export default function Home() {
   return (
@@ -8,5 +42,5 @@ export default function Home() {
         <meta name="description" content="Credit system application" />
       </Head>
     </div>
-  )
+  );
 }
