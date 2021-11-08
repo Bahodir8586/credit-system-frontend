4bf5311b605585d7abc0dd8f408161a687165707
Bahodir8586 <bahodira213@gmail.com>
2021-11-08 17:47:47 +0500

    admin routes first part

--- route-paths.js
+++ route-paths.js
@@ -6,7 +6,46 @@ const routePaths = {
   admin: {
     index: '/admin',
     profile: '/admin/profile',
-    warehouse: '/admin/warehouse',
+    employees: {
+      index: '/admin/employees',
+      id: '/admin/employees/id',
+      add: '/admin/employees/add',
+    },
+    shops: {
+      index: '/admin/shops',
+      add: '/admin/shops/add',
+      id: '/admin/shops/id',
+    },
+    warehouse: {
+      index: '/admin/warehouse',
+      history: {
+        in: {
+          index: '/admin/warehouse/in',
+          add: '/admin/warehouse/in/add',
+        },
+        out: {
+          index: '/admin/warehouse/out',
+          add: '/admin/warehouse/out/add',
+        },
+      },
+    },
+    manager: {
+      index: '/admin/managers',
+      id: '/admin/managers/id',
+      team: {
+        index: '/admin/manager/team',
+        sales: '/admin/manager/team/sales',
+        credits: '/admin/manager/team/credits',
+      },
+      sales: '/admin/manager/sales',
+      credits: '/admin/manager/credits',
+    },
+    assistant: {
+      index: '/admin/assistants',
+      id: '/admin/assistants/id',
+      sales: '/admin/assistants/sales',
+      credits: '/admin/assistants/credits',
+    },
   },
   manager: {
     index: '/manager',
