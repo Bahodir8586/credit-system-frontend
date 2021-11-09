fb192b5c70e58b19ec961ed9d240d2ef6c848a67
Bahodir8586 <bahodira213@gmail.com>
2021-11-09 18:28:28 +0500

    teams page admin 1st step

--- pages/admin/managers/index.js
+++ pages/admin/managers/index.js
@@ -4,6 +4,7 @@ import Cookies from 'js-cookie';
 import routePaths from '@/route-paths';
 import { isAuthenticated } from '@/utils/auth';
 import AdminLayout from '@/layouts/admin/AdminLayout';
+import EmployeeTable from '@/modules/admin/employeeTable';
 
 export async function getServerSideProps(context) {
   const data = await isAuthenticated(context);
@@ -34,7 +35,28 @@ export default function Home() {
         <title>Credit System</title>
         <meta name="description" content="Credit system application" />
       </Head>
-      <AdminLayout pageTitle="Teams"></AdminLayout>
+      <AdminLayout pageTitle="Teams">
+        <div className="flex flex-row mb-4 justify-center w-full">
+          <form className="flex md:flex-row justify-between w-full md:space-x-3 space-y-3 md:space-y-0 md:justify-center items-center">
+            <div className="font-medium text-xl mr-2 hidden md:inline-flex">Search</div>
+            <div className=" relative ">
+              <input
+                type="text"
+                id='"form-subscribe-Filter'
+                className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-72 py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                placeholder="name"
+              />
+            </div>
+            <button
+              className="flex-shrink-0 px-4 py-2 text-base font-semibold text-white bg-purple-600 rounded-lg shadow-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 focus:ring-offset-purple-200"
+              type="submit"
+            >
+              Filter
+            </button>
+          </form>
+        </div>
+        <EmployeeTable />
+      </AdminLayout>
     </div>
   );
 }
