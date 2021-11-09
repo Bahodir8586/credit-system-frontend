298e83a718bb8f89ea0040a7bdffd2b14ed4331d
Bahodir8586 <bahodira213@gmail.com>
2021-11-09 19:07:53 +0500

    employees page

--- pages/admin/employees/index.js
+++ pages/admin/employees/index.js
@@ -24,17 +24,40 @@ export async function getServerSideProps(context) {
       },
     };
   }
+  // TODO: Get employees list there
   return { props: {} };
 }
 
+const people = [
+  {
+    name: 'Jane Cooper',
+    title: 'Regional Paradigm Technician',
+    role: 'Manager',
+    email: 'jane.cooper@example.com',
+  },
+  {
+    name: 'Cody Fisher',
+    title: 'Product Directives Officer',
+    role: 'Manager',
+    email: 'cody.fisher@example.com',
+  },
+  // More people...
+];
+
 export default function Home() {
+  const search = (name) => {
+    // TODO: search user according to name
+  };
   return (
     <div>
       <Head>
         <title>Credit System</title>
         <meta name="description" content="Credit system application" />
       </Head>
-      <AdminLayout pageTitle="Employees"></AdminLayout>
+      <AdminLayout pageTitle="Employees">
+        <EmployeeFilter search={search} />
+        <EmployeeTable people={people} />
+      </AdminLayout>
     </div>
   );
 }
