48190cb5e98b06baa319c6d68650e47e30c0a590
Bahodir8586 <bahodira213@gmail.com>
2021-11-11 23:38:37 +0500

    product creation done

--- pages/admin/warehouse/history/in/add.js
+++ pages/admin/warehouse/history/in/add.js
@@ -5,6 +5,7 @@ import routePaths from '@/route-paths';
 import { isAuthenticated } from '@/utils/auth';
 import AdminLayout from '@/layouts/admin/AdminLayout';
 import WarehouseInForm from '@/modules/admin/warehouse/warehouseInForm';
+import axios from '@/utils/axios';
 
 export async function getServerSideProps(context) {
   const data = await isAuthenticated(context);
@@ -30,8 +31,23 @@ export async function getServerSideProps(context) {
 
 // TODO: In action for warehouse
 export default function Home() {
-  const submitForm = (name, amount, price, priceDiscount, description, image) => {
+  const submitForm = async (name, amount, price, priceDiscount, description, image) => {
     console.log(name, amount, price, priceDiscount, description, image);
+    const data = new FormData();
+    data.append('name', name);
+    data.append('amount', amount);
+    data.append('price', price);
+    data.append('priceDiscount', priceDiscount);
+    data.append('description', description);
+    data.append('image', image);
+    try {
+      const response = await axios.post('/products', data, {
+        headers: { 'Content-type': 'multipart/form-data' },
+      });
+      console.log(response);
+    } catch (e) {
+      console.log(e);
+    }
   };
   return (
     <div>
