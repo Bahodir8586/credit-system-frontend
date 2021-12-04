d6f1b9cb9a993e05721fdcff8964606d01b1fd00
Abdullayev Bahodir <bahodira213@gmail.com>
2021-12-04 23:56:54 +0500

    ui fixes

--- modules/admin/branches/Branches.jsx
+++ modules/admin/branches/Branches.jsx
@@ -5,16 +5,19 @@ const Branches = ({ branches }) => {
   console.log(branches);
   return (
     <div className="flex justify-center flex-wrap">
-      {branches.map((el) => (
-        <BranchCard
-          key={el._id}
-          name={el.name}
-          image={el.image}
-          address={el.location.address}
-          description={el.location.description}
-          numberOfEmployees={el.employees.length}
-        />
-      ))}
+      {branches.map((el) => {
+        const { branch, employees } = el;
+        return (
+          <BranchCard
+            key={branch._id}
+            name={branch.name}
+            image={branch.image}
+            address={branch.location.address}
+            description={branch.location.description}
+            numberOfEmployees={employees?.length}
+          />
+        );
+      })}
     </div>
   );
 };
--- modules/admin/branches/branchCard/BranchCard.jsx
+++ modules/admin/branches/branchCard/BranchCard.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const BranchCard = ({ name, address, description, image, numberOfEmployees }) => {
   return (
-    <div className="overflow-hidden shadow-lg rounded-lg h-96 w-60 md:w-80 cursor-pointer m-auto">
+    <div className="overflow-hidden shadow-lg rounded-lg h-96 w-60 md:w-80 cursor-pointer mx-auto my-6">
       <a href="#" className="w-full block h-full">
         <img alt={name} src={image} className="max-h-40 w-full object-cover" />
         <div className="bg-white dark:bg-gray-800 w-full p-4 h-full">
