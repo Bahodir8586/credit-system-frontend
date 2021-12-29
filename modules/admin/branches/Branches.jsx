e56e7d55a4838c8850588debd603ea96ef21d415
Abdullayev Bahodir <bahodira213@gmail.com>
2021-12-29 10:15:14 +0500

    link for branches

--- modules/admin/branches/Branches.jsx
+++ modules/admin/branches/Branches.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import Link from 'next/link';
 import BranchCard from './branchCard';
 
 const Branches = ({ branches }) => {
-  console.log(branches);
   return (
     <div className="flex justify-center flex-wrap">
       {branches.map((el) => {
         const { branch, employees } = el;
         return (
           <BranchCard
+            _id={branch._id}
             key={branch._id}
             name={branch.name}
             image={branch.image}
--- modules/admin/branches/branchCard/BranchCard.jsx
+++ modules/admin/branches/branchCard/BranchCard.jsx
@@ -1,20 +1,24 @@
 /* eslint-disable @next/next/no-img-element */
 import React from 'react';
+import Link from 'next/link';
+import routePaths from '@/route-paths';
 
-const BranchCard = ({ name, address, description, image, numberOfEmployees }) => {
+const BranchCard = ({ _id, name, address, description, image, numberOfEmployees }) => {
   return (
     <div className="overflow-hidden shadow-lg rounded-lg h-96 w-60 md:w-80 cursor-pointer mx-auto my-6">
-      <a href="#" className="w-full block h-full">
-        <img alt={name} src={image} className="max-h-40 w-full object-cover" />
-        <div className="bg-white dark:bg-gray-800 w-full p-4 h-full">
-          <h4 className="text-indigo-500 text-xl font-medium text-center">{name}</h4>
-          <p className="text-gray-800 dark:text-white text-md font-medium mb-2">
-            Number of employees: {numberOfEmployees}
-          </p>
-          <p className="text-gray-400 dark:text-gray-300 font-light text-md">{address}</p>
-          <p className="text-gray-400 dark:text-gray-300 font-light text-md">{description}</p>
-        </div>
-      </a>
+      <Link href={`${routePaths['admin']['branches']['index']}/${_id}`} passHref>
+        <a href="#" className="w-full block h-full">
+          <img alt={name} src={image} className="max-h-40 w-full object-cover" />
+          <div className="bg-white dark:bg-gray-800 w-full p-4 h-full">
+            <h4 className="text-indigo-500 text-xl font-medium text-center">{name}</h4>
+            <p className="text-gray-800 dark:text-white text-md font-medium mb-2">
+              Number of employees: {numberOfEmployees}
+            </p>
+            <p className="text-gray-400 dark:text-gray-300 font-light text-md">{address}</p>
+            <p className="text-gray-400 dark:text-gray-300 font-light text-md">{description}</p>
+          </div>
+        </a>
+      </Link>
     </div>
   );
 };
