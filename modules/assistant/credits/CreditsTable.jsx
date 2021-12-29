85546ec67c3c60c2771974f4730a816d749f563d
Abdullayev Bahodir <bahodira213@gmail.com>
2021-12-29 11:47:08 +0500

    init files

---
+++ modules/assistant/credits/CreditsTable.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CreditsTable = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default CreditsTable;
---
+++ modules/assistant/sales/SalesForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SalesForm = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default SalesForm;
