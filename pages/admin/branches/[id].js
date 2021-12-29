5d4890e029a88b08c538da971ddb1f2029432672
Abdullayev Bahodir <bahodira213@gmail.com>
2021-12-29 10:28:10 +0500

    single branch page

--- pages/admin/branches/[id].js
+++ pages/admin/branches/[id].js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 import Cookies from 'js-cookie';
 
 import AdminLayout from '@/layouts/admin/AdminLayout';
@@ -24,18 +25,37 @@ export async function getServerSideProps(context) {
       },
     };
   }
-  return { props: {} };
+  const jwt = data.jwt;
+
+  try {
+    // TODO: get single branch info there
+    const response = await fetch(`http://localhost:6060/api/branches/${context.params.id}`, {
+      headers: { authorization: `Bearer ${jwt}` },
+    });
+    const data = await response.json();
+    return { props: { branch: data?.data?.branch } };
+  } catch (error) {
+    console.log(error);
+    return {
+      redirect: {
+        destination: routePaths[data.role]['branches']['index'] ?? '/',
+        permanent: false,
+      },
+    };
+  }
 }
 
 // TODO: information about the single shop and its employees
-export default function Home() {
+export default function Home({ branch }) {
+  console.log(branch);
+  const router = useRouter();
   return (
     <div>
       <Head>
         <title>Credit System</title>
         <meta name="description" content="Credit system application" />
       </Head>
-      <AdminLayout pageTitle="Profile"></AdminLayout>
+      <AdminLayout pageTitle={branch.branch.name}></AdminLayout>
     </div>
   );
 }
